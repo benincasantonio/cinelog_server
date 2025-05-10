8a2c3e6153fa111eef2d81ede4acd529f77deee6
Antonio Benincasa <benincasaantonio98@gmail.com>
2025-05-10 11:58:54 +0200

    feat: Refactor User model to inherit from BaseEntity for common fields

--- app/models/user.py
+++ app/models/user.py
@@ -2,8 +2,10 @@ from mongoengine import Document, StringField, DateTimeField, ObjectIdField
 from bson import ObjectId
 from datetime import datetime
 
+from app.models.base_entity import BaseEntity
 
-class User(Document):
+
+class User(BaseEntity):
     id = ObjectIdField(primary_key=True, default=lambda: ObjectId())
     firstName = StringField(required=True)
     lastName = StringField(required=True)
@@ -11,7 +13,4 @@ class User(Document):
     handle = StringField(required=True, unique=True)
     password = StringField(required=True)
     dateOfBirth = DateTimeField()
-    deleted = StringField(default="false")
-    deletedAt = DateTimeField()
-    createdAt = DateTimeField(default=lambda: datetime.now())
 
