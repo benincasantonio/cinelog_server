14b17cac38ac6ff711e7ce6657fce63a52097ed0
Antonio Benincasa <benincasaantonio98@gmail.com>
2025-12-06 18:41:28 +0100

    refactor: update movie search endpoint to include authentication dependency and improve type hinting for return value

--- app/controllers/movie_controller.py
+++ app/controllers/movie_controller.py
@@ -1,8 +1,7 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Depends
 
-from app.repository.user_repository import UserRepository
-from app.schemas.auth_schemas import LoginRequest, RegisterRequest, RegisterResponse, LoginResponse
-from app.services.auth_service import AuthService
+from app.dependencies import auth_dependency
+from app.schemas.tmdb_schemas import TMDBMovieSearchResult
 from app.services.tmdb_service import TMDBService
 from app.utils.exceptions import AppException
 from os import getenv
@@ -14,7 +13,9 @@ tmdb_service = TMDBService(api_key=TMDB_API_KEY)
 
 
 @router.get("/search")
-def search_movies(query: str):
+def search_movies(
+    query: str, _: bool = Depends(auth_dependency)
+) -> TMDBMovieSearchResult:
     """
     Search for movies using TMDB API.
     """
