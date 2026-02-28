0d56d3aa03468e08a5571641ce48245476928703
Antonio Benincasa <benincasaantonio98@gmail.com>
2026-02-28 14:47:39 +0100

    chore(main): import app variable for Vercel ASGI application detection

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+from app import app  # noqa: E402, F401 - Vercel requires 'app' variable in this file
 
 # Note: The 'app' variable is automatically detected by Vercel as an ASGI application
