05f4a21a8506b6be8d92e511df77e68fb9d4fbba
Antonio Benincasa <benincasaantonio98@gmail.com>
2026-03-29 10:20:58 +0200

    refactor(types): remove __all__ declaration from user and log validation imports

--- app/types/__init__.py
+++ app/types/__init__.py
@@ -14,18 +14,3 @@ from app.types.log_validation import (
     WATCHED_WHERE_CHOICES,
     validate_watched_where,
 )
-
-__all__ = [
-    "BioStr",
-    "HandleStr",
-    "NameStr",
-    "OptionalHandleStr",
-    "OptionalNameStr",
-    "OptionalWatchedWhereStr",
-    "WATCHED_WHERE_CHOICES",
-    "WatchedWhereStr",
-    "sanitize_bio",
-    "validate_handle",
-    "validate_name",
-    "validate_watched_where",
-]
